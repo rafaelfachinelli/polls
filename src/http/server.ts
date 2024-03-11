5e4eb6ac096c18c110bfad6cd0dbbb179ac4bdae
rafaelfachinelli <rafael.fachinelli@hotmail.com>
2024-03-11 00:08:03 -0300

    feat: Add zod as a dependency and create migration and routes for creating and getting polls

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,11 @@
 import fastify from 'fastify'
+import { createPoll } from './routes/create-poll'
+import { getPoll } from './routes/get-poll'
 
 const app = fastify()
+
+app.register(createPoll)
+app.register(getPoll)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('Server is running on port 3333')
